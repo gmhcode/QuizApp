cb03221898ec1dce47950da1a117b401376570e3
gmhcode <greg.hughes@meritagepos.com>
2020-09-04 14:30:14 -0600

    first test commit

---
+++ app/src/main/java/com/example/quizapp/Constants.kt
@@ -0,0 +1,18 @@
+package com.example.quizapp
+
+object Constants  {
+    fun getQuestions(): ArrayList<Question>{
+        val questionsList = ArrayList<Question>()
+
+        val que1 = Question(1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina",
+            "Australia",
+            "Armenia",
+            "Austria",
+            1
+        )
+        questionsList.add(que1)
+        return questionsList
+    }
+}
